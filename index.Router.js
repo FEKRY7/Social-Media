215cdf630214d659b6ba8cec8659cc5755d92f64
FEKRY7 <159729804+FEKRY7@users.noreply.github.com>
2024-09-06 22:42:01 +0300

    Update index.Router.js

--- index.Router.js
+++ index.Router.js
@@ -1,1 +1,24 @@
+const express = require("express");
+
+const userRouter = require("./src/modules/auth/auth.router.js");
+const postRouter = require("./src/modules/Post/Posts.router.js");
+const commentRouter = require("./src/modules/Comment/Comments.router.js");
+const commentReplayRouter = require("./src/modules/CommentReplay/commentReplay.router.js");
+const mongodbconnect = require("./Database/dbConnection.js");
+
+const AppRouter = (app) => {
+  mongodbconnect();
+
+  //convert Buffer Data
+  // Middleware to parse JSON
+  app.use(express.json());
+
+  // Routes
+  app.use("/api/user", userRouter);
+  app.use("/api/post", postRouter);
+  app.use("/api/comment", commentRouter);
+  app.use("/api/commentReplay", commentReplayRouter);
+};
+
+module.exports = AppRouter;
 
