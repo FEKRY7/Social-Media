61e95892e62296026dc9cf524414b589fb87ab00
FEKRY7 <159729804+FEKRY7@users.noreply.github.com>
2024-09-06 22:41:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const cors = require('cors')
+const AppRouter = require("./index.Router.js");
+const cors = require("cors");
+require("dotenv").config();
 
-require('dotenv').config()
+app.use(cors());
 
-const mongodbconnect = require('./Database/dbConnection.js')
-mongodbconnect()
-
-app.use(express.json())
-app.use(cors())
-
-const userRouter = require('./src/modules/auth/auth.router.js')
-const postRouter = require('./src/modules/Post/Posts.router.js')
-const commentRouter = require('./src/modules/Comment/Comments.router.js')
-const commentReplayRouter = require('./src/modules/CommentReplay/commentReplay.router.js')
-
- 
-app.use('/api/user',userRouter)
-app.use('/api/post',postRouter)
-app.use('/api/comment',commentRouter)
-app.use('/api/commentReplay',commentReplayRouter)
-
+AppRouter(app);
 
 // Set up server to listen on specified port (default to 3000)
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-
-
 // 404 route
-app.use('*', (req, res) => {
-    res.status(404).json({ 'Msg': 'I Can\'t Found' });
+app.use("*", (req, res) => {
+  res.status(404).json({ Msg: "I Can't Found" });
 });
